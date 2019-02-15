9c32bf07f68df9d66e2755580608e9ba7640383b
Xavire La <xavierla74@hotmail.fr>
2019-02-15 15:48:39 +0100

    Création exo 2 et créatop, classe cartePaiement

---
+++ src/cartePaiement.java
@@ -0,0 +1,20 @@
+
+public class cartePaiement {
+
+	
+public double payeAchat(double montant)
+{
+	return montant;
+}
+public boolean autorisePaiement(double montant)
+{
+	boolean result;
+	result=false;
+	if (montant > 0)
+	{
+		result = true;
+	}
+	return result;
+}
+
+}
